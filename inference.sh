054299e32fe5d7956d55e77d4019f64d993eb53c
vlnguyen92 <nguyenvanlinh1992@gmail.com>
2017-05-12 11:52:42 -0400

    Split scripts to train and inference

---
+++ inference.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Set this to where you extracted the downloaded file
+export DATA_PATH=/scratch/wellman_fluxg/lvnguyen/seq2seq/data
+
+export VOCAB_SOURCE=${DATA_PATH}/vocab.bpe.32000
+export VOCAB_TARGET=${DATA_PATH}/vocab.bpe.32000
+export TRAIN_SOURCES=${DATA_PATH}/train.tok.clean.bpe.32000.en
+export TRAIN_TARGETS=${DATA_PATH}/train.tok.clean.bpe.32000.en
+export DEV_SOURCES=${DATA_PATH}/newstest2013.tok.bpe.32000.en
+export DEV_TARGETS=${DATA_PATH}/newstest2013.tok.bpe.32000.en
+
+export DEV_TARGETS_REF=${DATA_PATH}/newstest2013.tok.en
+export TRAIN_STEPS=50000
+
+export MODEL_DIR=${TMPDIR:-/scratch/wellman_fluxg/lvnguyen/seq2seq/trained_models}/nmt_tutorial
+mkdir -p $MODEL_DIR
+
+python -m bin.infer \
+  --tasks "
+    - class: DecodeText
+    - class: DumpBeams
+      params:
+        file: ${PRED_DIR}/beams.npz" \
+  --model_dir $MODEL_DIR \
+  --model_params "
+    inference.beam_search.beam_width: 5" \
+  --input_pipeline "
+    class: ParallelTextInputPipeline
+    params:
+      source_files:
+        - $DEV_SOURCES" \
+  > ${PRED_DIR}/predictions.txt
