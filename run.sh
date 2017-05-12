cbfc5f4e4728ab1d18fb77a5d101b65d11e13e25
vlnguyen92 <nguyenvanlinh1992@gmail.com>
2017-05-11 22:32:53 -0400

    Configured for flux

--- run.sh
+++ run.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Set this to where you extracted the downloaded file
-export DATA_PATH=./data
+export DATA_PATH=/scratch/wellman_fluxg/lvnguyen/seq2seq/data
 
 export VOCAB_SOURCE=${DATA_PATH}/vocab.bpe.32000
 export VOCAB_TARGET=${DATA_PATH}/vocab.bpe.32000
-export TRAIN_SOURCES=${DATA_PATH}/small_data.en
-export TRAIN_TARGETS=${DATA_PATH}/small_data.en
+export TRAIN_SOURCES=${DATA_PATH}/train.tok.clean.bpe.32000.en
+export TRAIN_TARGETS=${DATA_PATH}/train.tok.clean.bpe.32000.en
 export DEV_SOURCES=${DATA_PATH}/newstest2013.tok.bpe.32000.en
 export DEV_TARGETS=${DATA_PATH}/newstest2013.tok.bpe.32000.en
 
 export DEV_TARGETS_REF=${DATA_PATH}/newstest2013.tok.en
 export TRAIN_STEPS=50000
 
-export MODEL_DIR=${TMPDIR:-./trained_models}/nmt_tutorial
+export MODEL_DIR=${TMPDIR:-/scratch/wellman_fluxg/lvnguyen/seq2seq/trained_models}/nmt_tutorial
 mkdir -p $MODEL_DIR
 
 python -m bin.train \
