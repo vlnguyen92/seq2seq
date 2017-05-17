05bc8e588d3af84f39fd81624552e39f40096f67
Linh Van Nguyen <lvnguyen@ln001.conflux.arc-ts.umich.edu>
2017-05-16 22:00:49 -0400

    Inference on test set polarity

--- inference.sh
+++ inference.sh
@@ -9,13 +9,12 @@ export VOCAB_TARGET=${DATA_PATH}/vocab.bpe.32000
 export TRAIN_SOURCES=${DATA_PATH}/train.tok.clean.bpe.32000.en
 export TRAIN_TARGETS=${DATA_PATH}/train.tok.clean.bpe.32000.en
 #export DEV_SOURCES=${DATA_PATH}/newstest2013.tok.bpe.32000.en
-export DEV_SOURCES=${DATA_PATH}/polarity
-export DEV_TARGETS=${DATA_PATH}/newstest2013.tok.bpe.32000.en
+export DEV_SOURCES=${DATA_PATH}/test.tok.bpe.32000.en
+export DEV_TARGETS=${DATA_PATH}/test.tok.bpe.32000.en
 
-export DEV_TARGETS_REF=${DATA_PATH}/newstest2013.tok.en
-export TRAIN_STEPS=50000
+export DEV_TARGETS_REF=${DATA_PATH}/test.tok.en
 
-export MODEL_DIR=${TMPDIR:-./trained_models_full}/nmt_tutorial
+export MODEL_DIR=${TMPDIR:-./trained_models_full}/polarity_adv
 mkdir -p $MODEL_DIR
 
 export PRED_DIR=${MODEL_DIR}/pred
