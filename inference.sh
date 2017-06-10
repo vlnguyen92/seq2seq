d601feb92e9d783ad89f2bdc61872e6bd42fba60
Linh Van Nguyen <lvnguyen@ln001.conflux.arc-ts.umich.edu>
2017-06-10 01:03:32 -0400

    Added variables to inference script

--- inference.sh
+++ inference.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+INPUT_FILE=$1
+OUTPUT_FILE=$2
+
 # Set this to where you extracted the downloaded file
 #export DATA_PATH=/scratch/wellman_fluxg/lvnguyen/seq2seq/data
 export DATA_PATH=./nmt_data/wmt_polarity
 
-export DEV_SOURCES=${DATA_PATH}/newstest2013.tok.bpe.32000.en
-#export DEV_SOURCES=${DATA_PATH}/test.tok.bpe.32000.en
+#export DEV_SOURCES=${DATA_PATH}/newstest2013.tok.bpe.32000.en
+export DEV_SOURCES=${INPUT_FILE}
 
-export DEV_TARGETS_REF=${DATA_PATH}/test.tok.en
-
-export MODEL_DIR=${TMPDIR:-./trained_models_full}/polarity-full
+export MODEL_DIR=${TMPDIR:-./trained_models_full}/polarity_full
 mkdir -p $MODEL_DIR
 
 export PRED_DIR=${MODEL_DIR}/pred
@@ -28,5 +29,6 @@ python -m bin.infer \
     class: ParallelTextInputPipeline
     params:
       source_files:
-        - $DEV_SOURCES"
-  > ${PRED_DIR}/predictions_polarity.txt
+        - $DEV_SOURCES" \
+  > ${PRED_DIR}/${OUTPUT_FILE}
+#predictions_polarity.txt
